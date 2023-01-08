7872bed669a2fceb19408ba4039b231640550aeb
heqianjun00 <heqianjun00@163.com>
2023-01-08 20:45:39 +0800

    修改文件名

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,12 @@ const routes = [
   {
     path: "/home",
     name: "home",
-    component: () => import("@/views/home/index.vue"),
+    component: () => import("@/views/home/Index.vue"),
   },
   {
     path: "/expanding-cards",
     name: "expanding-cards",
-    component: () => import("@/views/01-expanding-cards/index.vue"),
+    component: () => import("@/views/01-expanding-cards/Index.vue"),
     meta: {
       title: "Expanding Cards"
     }
@@ -18,7 +18,7 @@ const routes = [
   {
     path: "/progress-steps",
     name: "progress-steps",
-    component: () => import("@/views/02-progress-steps/index.vue"),
+    component: () => import("@/views/02-progress-steps/Index.vue"),
     meta: {
       title: "Progress Steps"
     }
@@ -26,7 +26,7 @@ const routes = [
   {
     path: "/rotating-nav-animation",
     name: "rotating-nav-animation",
-    component: () => import("@/views/03-rotating-nav-animation/index.vue"),
+    component: () => import("@/views/03-rotating-nav-animation/Index.vue"),
     meta: {
       title: "Rotating Nav"
     }
@@ -34,7 +34,7 @@ const routes = [
   {
     path: "/hidden-search",
     name: "hidden-search",
-    component: () => import("@/views/04-hidden-search/index.vue"),
+    component: () => import("@/views/04-hidden-search/Index.vue"),
     meta: {
       id: 0,
       title: "Hidden Search"
@@ -43,7 +43,7 @@ const routes = [
   {
     path: "/blurry-loading",
     name: "blurry-loading",
-    component: () => import("@/views/05-blurry-loading/index.vue"),
+    component: () => import("@/views/05-blurry-loading/Index.vue"),
     meta: {
       id: 0,
       title: "Blurry Loading"
@@ -52,7 +52,7 @@ const routes = [
   {
     path: "/:pathMatch(.*)*",
     name: "error",
-    component: () => import("@/views/error/index.vue")
+    component: () => import("@/views/error/Index.vue")
   }
 ]
 
