27af0867cd8f6905302182010ac3e472a1a75a72
heqianjun00 <heqianjun00@163.com>
2023-01-08 20:37:37 +0800

    引入IconPark图标库

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 **参考该项目：**<https://github.com/bradtraversy/50projects50days>
 
 **首页按钮特效：**<https://juejin.cn/post/7167355169934409758>
+
+**IconPark字节图标库：**<https://iconpark.oceanengine.com/official>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router';
-import birdCard from '@/components/birdCard.vue';
+import BirdCard from '@/components/BirdCard.vue';
 
 const route = useRouter();
 const routesList = route.options.routes.filter((item) => item.meta);
